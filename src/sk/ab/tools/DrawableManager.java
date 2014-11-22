75922f6c8a1d78ae87906e8ae685a22153e49d8f
adrian <adrian.benko@gmail.com>
2014-11-22 23:27:48 +0100

    new DownloadManager

--- src/sk/ab/tools/DrawableManager.java
+++ src/sk/ab/tools/DrawableManager.java
@@ -2,13 +2,13 @@ package sk.ab.tools;
 
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
-import android.os.Handler;
-import android.os.Message;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +23,37 @@ public class DrawableManager {
     private Resources resources;
     private final Map<String, Drawable> drawableMap;
 
+    private class DownloadFilesTask extends AsyncTask<String, Void, Drawable> {
+        private String url;
+        private final WeakReference<ImageView> imageViewReference;
+
+        public DownloadFilesTask(final ImageView imageView) {
+            imageViewReference = new WeakReference<ImageView>(imageView);
+        }
+
+        protected Drawable doInBackground(String... urls) {
+            url = urls[0];
+            return fetchDrawable(url);
+        }
+
+        protected void onPostExecute(Drawable result) {
+            if (result != null) {
+
+                drawableMap.put(url, result);
+                Log.d(this.getClass().getSimpleName(), "got a drawable: " + result.getBounds() + ", "
+                        + result.getIntrinsicHeight() + "," + result.getIntrinsicWidth() + ", "
+                        + result.getMinimumHeight() + "," + result.getMinimumWidth());
+
+                if (imageViewReference != null) {
+                    ImageView imageView = imageViewReference.get();
+                    imageView.setImageDrawable(result);
+                }
+            } else {
+                Log.w(this.getClass().getSimpleName(), "could not get drawable");
+            }
+        }
+    }
+
     public DrawableManager(Resources resources) {
         this.resources = resources;
         this.drawableMap = new HashMap<String, Drawable>();
@@ -31,50 +62,32 @@ public class DrawableManager {
     public Drawable fetchDrawable(String urlString) {
         Log.d(this.getClass().getSimpleName(), "image url:" + urlString);
         try {
-            InputStream is = new java.net.URL(urlString).openStream();
+            InputStream inputStream = null;
+            try {
+                inputStream = new java.net.URL(urlString).openStream();
 
-            Drawable drawable = Drawable.createFromStream(is, "src");
+                Drawable drawable = Drawable.createFromStream(inputStream, "src");
 
-            if (drawable != null) {
-                drawableMap.put(urlString, drawable);
-                Log.d(this.getClass().getSimpleName(), "got a thumbnail drawable: " + drawable.getBounds() + ", "
-                        + drawable.getIntrinsicHeight() + "," + drawable.getIntrinsicWidth() + ", "
-                        + drawable.getMinimumHeight() + "," + drawable.getMinimumWidth());
-            } else {
-                Log.w(this.getClass().getSimpleName(), "could not get thumbnail");
+                return drawable;
+            } catch (MalformedURLException e) {
+                Log.e(this.getClass().getSimpleName(), "fetchDrawable failed", e);
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
             }
-
-            return drawable;
-        } catch (MalformedURLException e) {
-            Log.e(this.getClass().getSimpleName(), "fetchDrawable failed", e);
-            return null;
         } catch (IOException e) {
             Log.e(this.getClass().getSimpleName(), "fetchDrawable failed", e);
-            return null;
         }
+        return null;
     }
 
     public void fetchDrawableOnThread(final String urlString, final ImageView imageView) {
         if (drawableMap.containsKey(urlString)) {
             imageView.setImageDrawable(drawableMap.get(urlString));
         } else {
-            final Handler handler = new Handler() {
-                @Override
-                public void handleMessage(Message message) {
-                    imageView.setImageDrawable((Drawable) message.obj);
-                }
-            };
-
-            Thread thread = new Thread() {
-                @Override
-                public void run() {
-                    //TODO : set imageView to a "pending" image
-                    Drawable drawable = fetchDrawable(urlString);
-                    Message message = handler.obtainMessage(1, drawable);
-                    handler.sendMessage(message);
-                }
-            };
-            thread.start();
+            DownloadFilesTask task = new DownloadFilesTask(imageView);
+            task.execute(urlString);
         }
     }
 }
