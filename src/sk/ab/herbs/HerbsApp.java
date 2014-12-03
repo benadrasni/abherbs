49537d81dca47602ec00a02b995ca02e57677019
adrian <adrian.benko@gmail.com>
2014-12-03 19:28:26 +0100

    analytics support

---
+++ src/sk/ab/herbs/HerbsApp.java
@@ -0,0 +1,42 @@
+package sk.ab.herbs;
+
+import android.app.Application;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Logger;
+import com.google.android.gms.analytics.Tracker;
+
+import java.util.HashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: adrian
+ * Date: 12/1/14
+ * Time: 9:20 PM
+ * <p/>
+ */
+public class HerbsApp extends Application {
+    private static final String PROPERTY_ID = "UA-56892333-1";
+
+    public static int GENERAL_TRACKER = 0;
+
+    public enum TrackerName {
+        APP_TRACKER, // Tracker used only in this app.
+    }
+
+    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+
+    public HerbsApp() {
+        super();
+    }
+
+    public synchronized Tracker getTracker(TrackerName trackerId) {
+        if (!mTrackers.containsKey(trackerId)) {
+
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            analytics.getLogger().setLogLevel(Logger.LogLevel.VERBOSE);
+            Tracker t = analytics.newTracker(PROPERTY_ID);
+            mTrackers.put(trackerId, t);
+        }
+        return mTrackers.get(trackerId);
+    }
+}
