6f7b3e467cb897459b3a6328918edeb4a58ad27a
adrian <adrian.benko@gmail.com>
2015-02-15 11:18:25 +0100

    floating text around illustration

--- app/src/main/java/sk/ab/herbs/fragments/PlantCardsFragment.java
+++ app/src/main/java/sk/ab/herbs/fragments/PlantCardsFragment.java
@@ -1,12 +1,18 @@
 package sk.ab.herbs.fragments;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ListFragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +22,14 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import sk.ab.herbs.Constants;
 import sk.ab.herbs.HerbsApp;
 import sk.ab.herbs.Plant;
 import sk.ab.herbs.R;
 import sk.ab.herbs.activities.DisplayPlantActivity;
+import sk.ab.tools.Margin;
 import sk.ab.tools.Utils;
 
 public class PlantCardsFragment extends ListFragment {
@@ -31,6 +39,8 @@ public class PlantCardsFragment extends ListFragment {
     private static final int CARD_INFO = 1;
     private static final int CARD_GALLERY = 2;
     private static final int CARD_SOURCES = 3;
+
+    private static final int INFO_SECTIONS = 6;
 
     private int thumbnail_position;
 
@@ -199,14 +209,6 @@ public class PlantCardsFragment extends ListFragment {
                 case CARD_INFO:
                     convertView = LayoutInflater.from(getContext()).inflate(R.layout.plant_card_info, parent, false);
 
-                    ImageView drawing = (ImageView) convertView.findViewById(R.id.plant_background);
-                    drawing.setImageResource(android.R.color.transparent);
-                    if (plant.getBack_url() != null) {
-                        ImageLoader.getInstance().displayImage(plant.getBack_url(), drawing,
-                                ((HerbsApp)getActivity().getApplication()).getOptions());
-
-                    }
-
                     TextView firstRow = (TextView) convertView.findViewById(R.id.first_row);
                     StringBuilder firstRowText = new StringBuilder();
                     firstRowText.append(plant.getDescWithHighlight(getResources().getString(R.string.plant_height),
@@ -218,29 +220,50 @@ public class PlantCardsFragment extends ListFragment {
                     TextView upImage = (TextView) convertView.findViewById(R.id.up_image);
                     upImage.setText(Html.fromHtml(plant.getDescription()));
 
-                    TextView nextToImage = (TextView) convertView.findViewById(R.id.next_to_image);
+                    final int[][] spanIndex = new int[2][INFO_SECTIONS];
+                    final StringBuilder text = new StringBuilder();
+                    String[][] sections = { {getResources().getString(R.string.plant_flowers), plant.getFlower()},
+                            {getResources().getString(R.string.plant_inflorescences), plant.getInflorescence()},
+                            {getResources().getString(R.string.plant_fruits), plant.getFruit()},
+                            {getResources().getString(R.string.plant_leaves), plant.getLeaf()},
+                            {getResources().getString(R.string.plant_stem), plant.getStem()},
+                            {getResources().getString(R.string.plant_habitat), plant.getHabitat()}
+                    };
 
-                    StringBuilder text = new StringBuilder();
-                    text.append(plant.getDescWithHighlight(getResources().getString(R.string.plant_flowers),
-                            plant.getFlower()));
-                    text.append("<br/>");
-                    text.append(plant.getDescWithHighlight(getResources().getString(R.string.plant_inflorescences),
-                            plant.getInflorescence()));
-                    text.append("<br/>");
-                    text.append(plant.getDescWithHighlight(getResources().getString(R.string.plant_fruits),
-                            plant.getFruit()));
-                    text.append("<br/>");
-                    text.append(plant.getDescWithHighlight(getResources().getString(R.string.plant_leaves),
-                            plant.getLeaf()));
-                    text.append("<br/>");
-                    text.append(plant.getDescWithHighlight(getResources().getString(R.string.plant_stem),
-                            plant.getStem()));
+                    for(int i = 0; i < INFO_SECTIONS; i++ ) {
+                        spanIndex[0][i] = text.length();;
+                        spanIndex[1][i] = text.length() + sections[i][0].length();
+                        text.append(sections[i][0] + ": " + sections[i][1] + " ");
+                        //text.append("\n");
+                    }
 
-                    nextToImage.setText(Html.fromHtml(text.toString()));
+                    final TextView nextToImage = (TextView) convertView.findViewById(R.id.next_to_image);
+                    final ImageView drawing = (ImageView) convertView.findViewById(R.id.plant_background);
+                    Point size = new Point();
+                    getActivity().getWindowManager().getDefaultDisplay().getSize(size);
+                    drawing.setMaxWidth(size.x / 3);
+                    final SpannableString ss = new SpannableString(text.toString());
+                    for(int i = 0; i < INFO_SECTIONS; i++ ) {
+                        ss.setSpan(new StyleSpan(Typeface.BOLD), spanIndex[0][i], spanIndex[1][i],
+                                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    }
+                    if (plant.getBack_url() != null) {
+                        ImageLoader.getInstance().displayImage(plant.getBack_url(), drawing, new SimpleImageLoadingListener() {
+                            @Override
+                            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                                int leftMargin = loadedImage.getWidth() + 10;
+                                int height = loadedImage.getHeight();
+                                ss.setSpan(new Margin(height / (int) (nextToImage.getLineHeight() * nextToImage
+                                        .getLineSpacingMultiplier() + nextToImage.getLineSpacingExtra()) + 2,
+                                        leftMargin), 0, ss.length(), Spanned.SPAN_PARAGRAPH);
+                                nextToImage.setText(ss);
+                            }
+                        });
 
-                    TextView bellowImage = (TextView) convertView.findViewById(R.id.bellow_image);
-                    bellowImage.setText(Html.fromHtml(plant.getDescWithHighlight(getResources()
-                                    .getString(R.string.plant_habitat), plant.getHabitat())));
+                    } else {
+                        nextToImage.setText(ss);
+                    }
+
                     break;
                 case CARD_GALLERY:
                     convertView = LayoutInflater.from(getContext()).inflate(R.layout.plant_card_gallery, parent, false);
