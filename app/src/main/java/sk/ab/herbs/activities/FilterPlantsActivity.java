e3de520f1cf069a3d7fc071ccad906d3f4628cd9
adrian <adrian.benko@gmail.com>
2016-04-10 21:35:47 +0200

    1.2.7

--- app/src/main/java/sk/ab/herbs/activities/FilterPlantsActivity.java
+++ app/src/main/java/sk/ab/herbs/activities/FilterPlantsActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +18,6 @@ import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
 import com.github.amlcurran.showcaseview.ShowcaseView;
-import com.github.amlcurran.showcaseview.SimpleShowcaseEventListener;
 import com.github.amlcurran.showcaseview.targets.ViewTarget;
 
 import java.util.ArrayList;
