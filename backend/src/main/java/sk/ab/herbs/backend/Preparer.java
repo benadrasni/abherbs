44aa1a6fc137e956ba1de4f8a4caa07c3821c7ae
adrian <adrian.benko@gmail.com>
2016-06-02 22:08:25 +0200

    1.3.2 - plant entity

---
+++ backend/src/main/java/sk/ab/herbs/backend/Preparer.java
@@ -0,0 +1,164 @@
+package sk.ab.herbs.backend;
+
+import com.google.appengine.repackaged.com.google.gson.JsonArray;
+import com.google.appengine.repackaged.com.google.gson.JsonElement;
+import com.google.appengine.repackaged.com.google.gson.JsonObject;
+import com.google.appengine.repackaged.com.google.gson.JsonParser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+/**
+ * Created by adrian on 4.5.2016.
+ */
+public class Preparer {
+
+    public static void main(String[] params) {
+
+        try {
+            String name = "Galanthus nivalis";
+            Document doc = Jsoup.connect("https://species.wikimedia.org/w/index.php?title=" + name + "&action=edit").get();
+
+            String wikiPage = doc.getElementsByTag("textarea").val();
+            String vn = wikiPage.substring(wikiPage.indexOf("{{VN"), wikiPage.indexOf("}}", wikiPage.indexOf("{{VN"))).replace("\n", "");
+            String[] vnItems = vn.substring(5, vn.length()).split("\\|");
+
+            Map<String, List<String>> result = new TreeMap<>();
+            List<String> latin = new ArrayList<>();
+            latin.add(name);
+            result.put("la", latin);
+
+            for(String vnItem : vnItems) {
+                String[] hlp = vnItem.split("=");
+                if (hlp.length > 1) {
+                    String language = hlp[0];
+
+                    String[] names = hlp[1].trim().split(", ");
+                    if (names.length == 1) {
+                        names = hlp[1].split(" / ");
+                    }
+                    List<String> speciesValuesOld = new ArrayList<>(Arrays.asList(names));
+                    List<String> speciesValues = new ArrayList<>();
+                    for(String speciesValue : speciesValuesOld) {
+                        if (!name.toLowerCase().equals(speciesValue.toLowerCase())) {
+                            speciesValues.add(speciesValue.trim());
+                        }
+                    }
+
+                    if (speciesValues.size() > 0) {
+                        result.put(language, speciesValues);
+                    }
+
+                }
+            }
+
+            String id = getWikidata(name);
+
+
+            URL url = new URL("https://www.wikidata.org/wiki/Special:EntityData/" + id + ".json");
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject wikidata = root.getAsJsonObject().getAsJsonObject("entities").getAsJsonObject(id);
+
+            JsonObject labels = wikidata.getAsJsonObject("labels");
+            JsonObject aliases = wikidata.getAsJsonObject("aliases");
+
+            for (Map.Entry<String,JsonElement> entry : labels.entrySet()) {
+                JsonObject value = entry.getValue().getAsJsonObject();
+
+                List<String> names = result.get(value.get("language").getAsString());
+                List<String> namesLower = new ArrayList<>();
+                if (names == null) {
+                    names = new ArrayList<>();
+                    result.put(value.get("language").getAsString(), names);
+                }
+                for (String onename : names) {
+                    namesLower.add(onename.toLowerCase());
+                }
+
+                if (!namesLower.contains(value.get("value").getAsString().toLowerCase()) && !name.toLowerCase().equals(value.get("value").getAsString().toLowerCase())) {
+                    names.add(value.get("value").getAsString());
+                }
+            }
+
+            for (Map.Entry<String,JsonElement> entry : aliases.entrySet()) {
+                JsonArray value = entry.getValue().getAsJsonArray();
+
+                List<String> names = result.get(entry.getKey());
+                List<String> namesLower = new ArrayList<>();
+                if (names == null) {
+                    names = new ArrayList<>();
+                    result.put(entry.getKey(), names);
+                }
+                for(String onename : names) {
+                    namesLower.add(onename.toLowerCase());
+                }
+
+                for(JsonElement v : value) {
+                    if (!namesLower.contains(v.getAsJsonObject().get("value").getAsString().toLowerCase()) && !name.toLowerCase().equals(v.getAsJsonObject().get("value").getAsString().toLowerCase())) {
+                        names.add(v.getAsJsonObject().get("value").getAsString());
+                    }
+                }
+            }
+
+            for(Map.Entry<String, List<String>> entry : result.entrySet()) {
+                String key = entry.getKey();
+                List<String> value = entry.getValue();
+
+                if (value != null && value.size() > 0 && !key.startsWith("kk-") && !key.startsWith("zh-sg") && !key.startsWith("zh-hk") && !key.startsWith("zh-cn")) {
+                    System.out.print("|" + key + "=");
+
+                    StringBuilder names = new StringBuilder();
+                    for (String onename : value) {
+                        if (names.length() > 0) {
+                            names.append(", ");
+                        }
+                        names.append(onename);
+                    }
+                    System.out.println(names.toString());
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private static String getWikidata(String name) {
+        try {
+            Document doc = Jsoup.connect("https://species.wikimedia.org/wiki/" + name).get();
+
+            String wikiPage = doc.getElementsByAttributeValue("title", "Edit interlanguage links").attr("href");
+
+            return wikiPage.substring(wikiPage.lastIndexOf("/")+1, wikiPage.indexOf("#"));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
--- backend/src/main/java/sk/ab/herbs/backend/TaxonomyEndpoint.java
+++ backend/src/main/java/sk/ab/herbs/backend/TaxonomyEndpoint.java
@@ -10,14 +10,24 @@ import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.repackaged.com.google.gson.JsonArray;
+import com.google.appengine.repackaged.com.google.gson.JsonElement;
+import com.google.appengine.repackaged.com.google.gson.JsonObject;
+import com.google.appengine.repackaged.com.google.gson.JsonParser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Named;
 
@@ -54,7 +64,7 @@ public class TaxonomyEndpoint {
         if (wikiName == null) {
             wikiName = name;
         }
-        modifyEntity(taxonomyEntity, wikiName, "");
+        modifyEntityWikiSpecies(taxonomyEntity, wikiName);
         datastore.put(taxonomyEntity);
 
         return taxonomyEntity;
@@ -82,9 +92,12 @@ public class TaxonomyEndpoint {
             Entity entity = genuses.get(0);
 
             plantEntity.setProperty("taxonomyKey", entity.getKey());
-            modifyEntity(plantEntity, taxonomyName, "name_");
-
             plantEntity.setProperty("wikidata", getWikidata(taxonomyName));
+
+            modifyEntityWikiSpeciesPlant(plantEntity, taxonomyName);
+
+            //modifyEntityWikiData(plantEntity);
+            //modifyEntityWikiSpeciesAfterWikidata(plantEntity, taxonomyName);
 
             datastore.put(plantEntity);
         }
@@ -155,25 +168,100 @@ public class TaxonomyEndpoint {
         return results;
     }
 
-    private void modifyEntity(Entity entity, String name, String prefix) {
+    private void modifyEntityWikiSpeciesAfterWikidata(Entity entity, String latinName) {
         try {
-            Document doc = Jsoup.connect("https://species.wikimedia.org/w/index.php?title=" + name + "&action=edit").get();
+            List<String> latinAliases = (List<String>) entity.getProperty("aliases-la");
+            if (latinAliases == null) {
+                latinAliases = new ArrayList<>();
+            }
+            List<String> latinAliasesLower = new ArrayList<>();
+            for (String latinAlias : latinAliases) {
+                latinAliasesLower.add(latinAlias.toLowerCase());
+            }
+
+            for(String key: entity.getProperties().keySet()) {
+                if (key.startsWith("aliases-")) {
+                    List<String> aliasesOld = (List<String>) entity.getProperty(key);
+                    List<String> aliases = new ArrayList<>();
+                    for(String alias : aliasesOld) {
+                        if (!alias.toLowerCase().equals(latinName.toLowerCase()) && !latinAliasesLower.contains(alias.toLowerCase())) {
+                            aliases.add(alias);
+                        }
+                    }
+                    if (aliases.size() > 0) {
+                        entity.setProperty(key, aliases);
+                    } else {
+                        entity.removeProperty(key);
+                    }
+                }
+            }
+
+            Document doc = Jsoup.connect("https://species.wikimedia.org/w/index.php?title=" + latinName + "&action=edit").get();
 
             String wikiPage = doc.getElementsByTag("textarea").val();
             String vn = wikiPage.substring(wikiPage.indexOf("{{VN"), wikiPage.indexOf("}}", wikiPage.indexOf("{{VN"))).replace("\n", "");
-            String[] languages = vn.substring(5, vn.length()-2).split("\\|");
-            for(String language : languages) {
-                String[] hlp = language.split("=");
+            String[] vnItems = vn.substring(5, vn.length()-2).split("\\|");
+            for(String vnItem : vnItems) {
+                String[] hlp = vnItem.split("=");
                 if(hlp.length > 1) {
-                    String[] multiValue = hlp[1].trim().split(", ");
-                    if (multiValue.length > 1) {
-                        entity.setProperty(prefix+hlp[0].trim(), Arrays.asList(multiValue));
+                    String language = hlp[0].trim();
+
+                    String[] names = hlp[1].trim().split(", ");
+                    if (names.length == 1) {
+                        names = hlp[1].split(" / ");
+                    }
+
+                    // remove latin names from species values
+                    List<String> speciesValuesOld = new ArrayList<>(Arrays.asList(names));
+                    List<String> speciesValues = new ArrayList<>();
+                    for(String speciesValue : speciesValuesOld) {
+                        if (!speciesValue.toLowerCase().equals(latinName.toLowerCase()) && !latinAliasesLower.contains(speciesValue.toLowerCase())) {
+                            speciesValues.add(speciesValue);
+                        }
+                    }
+
+                    if (entity.getProperty("label-"+language) == null) {
+                        if (speciesValues.size() > 0) {
+                            entity.setProperty("label-"+language, speciesValues.get(0));
+                            speciesValues.remove(0);
+                            if (speciesValues.size() > 0) {
+                                entity.setProperty("aliases-"+language, speciesValues);
+                            }
+                        }
                     } else {
-                        multiValue = hlp[1].split(" / ");
-                        if (multiValue.length > 1) {
-                            entity.setProperty(prefix+hlp[0].trim(), Arrays.asList(multiValue));
-                        } else {
-                            entity.setProperty(prefix+hlp[0].trim(), hlp[1].trim());
+                        String label = entity.getProperty("label-"+language).toString();
+
+                        if (label.equals(latinName) && !language.equals("la")) {
+                            if (speciesValues.size() > 0) {
+                                entity.setProperty("label-" + language, speciesValues.get(0));
+                                speciesValues.remove(0);
+                            }
+                        }
+
+                        if (entity.getProperty("aliases-"+language) != null) {
+                            List<String> aliasesOld = (List<String>) entity.getProperty("aliases-" + language);
+                            List<String> aliases = new ArrayList<>();
+                            List<String> aliasesLower = new ArrayList<>();
+                            for(String alias : aliasesOld) {
+                                if (!alias.toLowerCase().equals(latinName.toLowerCase()) && !latinAliasesLower.contains(alias.toLowerCase())) {
+                                    aliases.add(alias);
+                                    aliasesLower.add(alias.toLowerCase());
+                                }
+                            }
+
+                            for(String value : speciesValues) {
+                                if (!aliasesLower.contains(value.toLowerCase()) && !label.toLowerCase().equals(value.toLowerCase())) {
+                                    aliases.add(value);
+                                    aliasesLower.add(value.toLowerCase());
+                                }
+                            }
+                            if (aliases.size() > 0) {
+                                entity.setProperty("aliases-" + language, aliases);
+                            } else {
+                                entity.removeProperty("aliases-" + language);
+                            }
+                        } else if (speciesValues.size() > 0) {
+                            entity.setProperty("aliases-"+language, speciesValues);
                         }
                     }
                 }
@@ -183,6 +271,154 @@ public class TaxonomyEndpoint {
         }
 
     }
+
+    private void modifyEntityWikiSpeciesPlant(Entity entity, String name) {
+        try {
+            Document doc = Jsoup.connect("https://species.wikimedia.org/w/index.php?title=" + name + "&action=edit").get();
+
+            String wikiPage = doc.getElementsByTag("textarea").val();
+            String vn = wikiPage.substring(wikiPage.indexOf("{{VN"), wikiPage.indexOf("}}", wikiPage.indexOf("{{VN"))).replace("\n", "");
+            String[] vnItems = vn.substring(5, vn.length()).split("\\|");
+            for(String vnItem : vnItems) {
+                String[] hlp = vnItem.split("=");
+                if(hlp.length > 1) {
+                    String language = hlp[0];
+
+                    String[] names = hlp[1].trim().split(", ");
+                    if (names.length == 1) {
+                        names = hlp[1].split(" / ");
+                    }
+                    List<String> speciesValuesOld = new ArrayList<>(Arrays.asList(names));
+                    List<String> speciesValues = new ArrayList<>();
+                    for(String speciesValue : speciesValuesOld) {
+                        speciesValues.add(speciesValue.trim());
+                    }
+                    if (speciesValues.size() > 1) {
+                        entity.setProperty("label-"+language, speciesValues.get(0));
+                        speciesValues.remove(0);
+                        if (speciesValues.size() > 0) {
+                            entity.setProperty("aliases-"+language, speciesValues);
+                        }
+                    } else {
+                        entity.setProperty("label-"+language, hlp[1].trim());
+                    }
+                }
+            }
+
+            String synonyms = wikiPage.substring(wikiPage.indexOf("===Synonyms==="), wikiPage.indexOf("==", wikiPage.indexOf("===Synonyms===")+14));
+            String[] lines = synonyms.split("\n");
+
+            String key = "Synonyms";
+            List<String> synonymList = new ArrayList<>();
+            for(String line : lines) {
+                if (line.trim().length() > 0) {
+                    if (line.trim().equals("{{HOT}}")) {
+                        if (synonymList.size() > 0) {
+                            entity.setProperty(key, synonymList);
+                        }
+                        key = "Homotypic";
+                        synonymList = new ArrayList<>();
+                        continue;
+                    } else if (line.trim().equals("{{HET}}")) {
+                        if (synonymList.size() > 0) {
+                            entity.setProperty(key, synonymList);
+                        }
+                        key = "Heterotypic";
+                        synonymList = new ArrayList<>();
+                        continue;
+                    } else if (line.trim().equals("{{BA}}")) {
+                        if (synonymList.size() > 0) {
+                            entity.setProperty(key, synonymList);
+                        }
+                        key = "Basionym";
+                        synonymList = new ArrayList<>();
+                        continue;
+                    }
+
+                    if (line.startsWith("** ''")) {
+                        String synonym = line.substring(5, line.indexOf("''", 5));
+                        synonymList.add(synonym);
+                    }
+                }
+            }
+            if (synonymList.size() > 0) {
+                entity.setProperty(key, synonymList);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void modifyEntityWikiSpecies(Entity entity, String name) {
+        try {
+            Document doc = Jsoup.connect("https://species.wikimedia.org/w/index.php?title=" + name + "&action=edit").get();
+
+            String wikiPage = doc.getElementsByTag("textarea").val();
+            String vn = wikiPage.substring(wikiPage.indexOf("{{VN"), wikiPage.indexOf("}}", wikiPage.indexOf("{{VN"))).replace("\n", "");
+            String[] languages = vn.substring(5, vn.length()).split("\\|");
+            for(String language : languages) {
+                String[] hlp = language.split("=");
+                if(hlp.length > 1) {
+                    String[] multiValue = hlp[1].trim().split(", ");
+                    if (multiValue.length > 1) {
+                        entity.setProperty(hlp[0].trim(), Arrays.asList(multiValue));
+                    } else {
+                        multiValue = hlp[1].split(" / ");
+                        if (multiValue.length > 1) {
+                            entity.setProperty(hlp[0].trim(), Arrays.asList(multiValue));
+                        } else {
+                            entity.setProperty(hlp[0].trim(), hlp[1].trim());
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void modifyEntityWikiData(Entity entity) {
+        try {
+            String id = entity.getProperty("wikidata").toString();
+
+            URL url = new URL("https://www.wikidata.org/wiki/Special:EntityData/" + id + ".json");
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject wikidata = root.getAsJsonObject().getAsJsonObject("entities").getAsJsonObject(id);
+
+            JsonObject labels = wikidata.getAsJsonObject("labels");
+            JsonObject aliases = wikidata.getAsJsonObject("aliases");
+
+            for (Map.Entry<String,JsonElement> entry : labels.entrySet()) {
+                JsonObject value = entry.getValue().getAsJsonObject();
+
+                entity.setProperty("label-"+value.get("language").getAsString(), value.get("value").getAsString());
+            }
+
+            for (Map.Entry<String,JsonElement> entry : aliases.entrySet()) {
+                JsonArray value = entry.getValue().getAsJsonArray();
+
+                List<String> aliasList = new ArrayList();
+                for(JsonElement v : value) {
+                    aliasList.add(v.getAsJsonObject().get("value").getAsString());
+                }
+
+                entity.setProperty("aliases-"+entry.getKey(), aliasList);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     private String getWikidata(String name) {
         try {
