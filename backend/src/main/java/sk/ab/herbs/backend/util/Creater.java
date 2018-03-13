5e86fac199a87ab635f64a5f97f34a555def43c6
benadrasni <adrian.benko@gmail.com>
2018-03-12 22:11:42 -0500

    new plants

--- backend/src/main/java/sk/ab/herbs/backend/util/Creater.java
+++ backend/src/main/java/sk/ab/herbs/backend/util/Creater.java
@@ -303,7 +303,7 @@ public class Creater {
             File file = new File(PATH_TO_PLANTS_TO_ADD);
 
             Scanner scan = new Scanner(file);
-            Integer plantId = 859;
+            Integer plantId = 864;
             while(scan.hasNextLine()){
                 final String plantName = scan.nextLine();
                 final String wikiSpeciesName = plantName;
